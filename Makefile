ba344c15cf521c81c472ee4b09bf7913708094ea
Jonah Sprandel <jsprandel@apecor.com>
2023-07-16 22:38:47 -0400

    minor changes

--- Makefile
+++ Makefile
@@ -14,7 +14,10 @@ compile_host: directories
 	$(HOST_COMPILER) $(HOST_SOURCES)
 
 compile_emb: directories
-	$(EMB_COMPILER) $(EMB_SOURCES)
+	$(EMB_COMPILER) --include_path=/opt/ti/ccs/ccs_base/msp430/include/ --include_path=/home/app --include_path=/home/dev/ti_cgt_msp430_21.6.1.LTS/include --define=__MSP430FR6989__ $(EMB_SOURCES)
+
+link_emb: directories
+	$(EMB_COMPILER) --use_hw_mpy=F5 --define=__MSP430FR6989__ --define=_MPU_ENABLE --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 -z -i"/opt/ti/ccs/ccs_base/msp430/include/" -i"/opt/ti/ccs/ccs_base/msp430/lib/5xx_6xx_FRxx/" -i"/opt/ti/ccs/ccs_base/msp430/lib/FR59xx" -i"/home/dev/ti_cgt_msp430_21.6.1.LTS/lib/" -i"/home/dev/ti_cgt_msp430_21.6.1.LTS/include" --rom_model --output_file=main.out "main.obj" "/opt/ti/ccs/ccs_base/msp430/include/lnk_msp430fr6989.cmd"
 
 directories:
 	mkdir -p build
--- src/app/main.c
+++ src/app/main.c
@@ -1,48 +1,54 @@
-// Flashing the LED with Timer_A, up mode, via polling
-#include "/opt/ti/ccs/ccs_base/msp430/include/msp430fr6989.h"
+// Timer_A up mode, with interrupt, flashes LEDs
+#include <msp430.h>
 #define redLED BIT0 // Red LED at P1.0
 #define greenLED BIT7 // Green LED at P9.7
 
-void main(void) {
-    // Stop the Watchdog timer
-    WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
-    // Unlock the GPIO pins
-    PM5CTL0 &= ~LOCKLPM5; // Enable the GPIO pins
-    // Configure the LEDs as output
-    P1DIR |= redLED; // Direct pin as output
-    P9DIR |= greenLED; // Direct pin as output
-    P1OUT &= ~redLED; // Turn LED Off
-    P9OUT &= ~greenLED; // Turn LED Off
-    // Configure ACLK to the 32 KHz crystal (function call)
-    void config_ACLK_to_32KHz_crystal();
-    // Configure Timer_A
-    /// Flashing the red LED with Timer_A, up mode, via polling
-    TA0CCR0 = 32678-1;
-    TA0CTL = TASSEL_1 | ID_0 | MC_1 | TACLR;
-    // Ensure flag is cleared at the start
-    TA0CTL &= ~TAIFG;
-    // Infinite loop
-    for(;;) {
-        // Wait in this empty loop for the flag to raise
-        while((TA0CTL & TAIFG) == 0 ) {}
-        // Do the action here
-        P1OUT ^= redLED;
-        TA0CTL &= ~TAIFG;
-    }
-}
-
-// Configures ACLK to 32 KHz crystal
 void config_ACLK_to_32KHz_crystal() {
     // By default, ACLK runs on LFMODCLK at 5MHz/128 = 39 KHz
     // Reroute pins to LFXIN/LFXOUT functionality
-     PJSEL1 &= ~BIT4;
-     PJSEL0 |= ~BIT4;
+    PJSEL1 &= ~BIT4;
+    PJSEL0 |= BIT4;
     // Wait until the oscillator fault flags remain cleared
-     CSCTL0 = CSKEY; // Unlock CS registers
-     do {
-     CSCTL5 &= ~LFXTOFFG; // Local fault flag
-     SFRIFG1 &= ~OFIFG; // Global fault flag
-     } while((CSCTL5 & LFXTOFFG) != 0);
-     CSCTL0_H = 0; // Lock CS registers
+    CSCTL0 = CSKEY; // Unlock CS registers
+    do {
+        CSCTL5 &= ~LFXTOFFG; // Local fault flag
+        SFRIFG1 &= ~OFIFG; // Global fault flag
+    } while((CSCTL5 & LFXTOFFG) != 0);
+    CSCTL0_H = 0; // Lock CS registers
     return;
+}
+
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD; // Stop the Watchdog timer
+    PM5CTL0 &= ~LOCKLPM5; // Enable the GPIO pins
+
+    P1DIR |= redLED; // Direct pin as output
+    P9DIR |= greenLED; // Direct pin as output
+    P1OUT &= ~redLED; // Turn LED Off
+    P9OUT |= greenLED; // Turn LED on
+
+    // Configure ACLK to the 32 KHz crystal
+    config_ACLK_to_32KHz_crystal();
+
+    // Configure Channel 0 for up mode with interrupt
+    TA0CCR0 |= 3276; // Fill to get 1 second @ 32kHz; to get 0.5 second delay, set to 16383; to get 0.1 second delay, set to 3276
+    TA0CCTL0 |= CCIE; // Enable Channel 0 CCIE bit
+    TA0CCTL0 &= ~CCIFG;  // Clear Channel 0 CCIFG bit
+
+    // Timer_A configuration (fill the line below)
+    // Use ACLK, divide by 1, up mode, TAR cleared (leaves TAIE=0)
+    TA0CTL = TASSEL_1 | ID_0 | MC_1 | TACLR;
+
+    // Enable the global interrupt bit (call an intrinsic function)
+    _enable_interrupts();
+
+    // Infinite loop... the code waits here between interrupts
+    for(;;) {}
+}
+
+#pragma vector = TIMER0_A0_VECTOR // Link the ISR to the vector
+__interrupt void T0A0_ISR() {
+    // Toggle both LEDs
+    P1OUT ^= redLED;
+    P9OUT ^= greenLED;
 }
